14fc0539faa90ec3137d114497877ccc84af8998
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-10-05 20:27:46 +0900

    Add_details_component_forms

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -120,5 +120,9 @@ export class HousingService {
     );
   }
 
-  constructor() {}
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`
+    );
+  }
 }
