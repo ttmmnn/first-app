cf1a8acccd38dd046ac6ea80e42faf4f32591bcd
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-10-05 21:46:07 +0900

    Add_検索機能

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -110,7 +110,7 @@ export class HousingService {
     },
   ];
 
-  getAllHousingLocatins(): HousingLocation[] {
+  getAllHousingLocations(): HousingLocation[] {
     return this.housingLocationList;
   }
 
